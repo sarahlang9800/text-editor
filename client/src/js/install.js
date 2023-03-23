43a3945fac80f71f9bd5d5f39784c1acb4dc1529
[Sarah Lang] <[lynnlang98@gmail.com]>
2023-03-23 17:46:07 -0400

    PWA Event Handlers

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,36 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Added an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    // Stores the triggered events
+    window.deferredPrompt = event;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    // Removes the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
+  });
+
+// Implemented a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+  
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+   return;
+  }
+
+  // Shows prompt
+  promptEvent.prompt();
+  
+  // Resets the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
+});
+
+// Added an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    // Clears prompt
+    window.deferredPrompt = null;
+  }); 
